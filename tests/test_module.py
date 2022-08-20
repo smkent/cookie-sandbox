48c184b3be620130308f2fb388d3791301e16d55
Stephen Kent <smkent@smkent.net>
2022-08-19 23:22:41 -0700

    Add test failures

--- tests/test_module.py
+++ tests/test_module.py
@@ -3,3 +3,15 @@ def test_import() -> None:
 
     assert cookie_sandbox
     assert cookie_sandbox.version
+
+
+def test_bad() -> None:
+    assert 1 == 2
+
+
+def test_good() -> None:
+    assert "OK"
+
+
+def test_also_bad() -> None:
+    assert True is False
